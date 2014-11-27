50b611ec088dc88b5e5f33118cf75e939e7e9c5c
josegallegos07 <jose.gallegos@brainplasticity.com>
2014-11-26 20:19:50 -0800

    added get_news_for_app method

---
+++ R/get-news-for-app.R
@@ -0,0 +1,37 @@
+#' Get news for an app
+#'
+#' Get the latest news for an app.
+#'
+#' @param app_id The AppID of the game.
+#' @param count The number of news entries to return. 
+#'   Returns all news entries if unspecified.
+#' @param max_length The maximum length of each news entry.
+#'   Returns all characters if unspecified.
+#'
+#' @return The latest news of a game specified by its AppID.
+#'
+#' Each item in the resulting list contains: a gid, a title, a url, 
+#' a shortened excerpt of the contents (to \code{max_length} characters), 
+#' terminated by "..." if longer than \code{max_length}, feed
+#' a feedlabel, a UNIX timestamp date, an author, and the feedname.
+#'
+#' @examples
+#' # get the most recent Binding of Isaac: Rebirth news post
+#' get_news_for_app(250900, count = 1)
+
+get_news_for_app <- function(app_id, count, max_length) {
+  if (missing(count)) {
+    count = ""
+  } else {
+  	count = paste(ARG_COUNT, count, sep = "")
+  }
+  if (missing(max_length)) {
+    max_length = ""
+  } else {
+    max_length = paste(ARG_MAX_LENGTH, max_length, sep = "")
+  }
+  args = paste(ARG_APP_ID, app_id, count, max_length, ARG_FORMAT, sep = "")
+  url = paste(STEAM_API_NEWS, args, sep= "")
+  json = get_json(url)
+  return (json$appnews$newsitems)
+}
--- R/service-definitions.R
+++ R/service-definitions.R
@@ -1,1 +1,7 @@
 STEAM_API_APP_LIST <- "http://api.steampowered.com/ISteamApps/GetAppList/v2"
+STEAM_API_NEWS <- "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/"
+
+ARG_APP_ID <- "?appid="
+ARG_MAX_LENGTH <- "&maxlength="
+ARG_COUNT <- "&count="
+ARG_FORMAT <- "&format=JSON"
