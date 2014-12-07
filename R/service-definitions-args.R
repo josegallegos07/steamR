97808539b1bbdea401280250d1f029d4a9ebde13
josegallegos07 <jose.gallegos@brainplasticity.com>
2014-12-07 15:56:19 -0800

    move arg definitions to own file

---
+++ R/service-definitions-args.R
@@ -0,0 +1,30 @@
+#' Steam Web API service definition argument constants
+#'
+#' Service definition argument constants for Steam Web API
+#'
+#' @name steam_api_args
+#' @format a character string
+#' @source \url{https://developer.valvesoftware.com/wiki/Steam_Web_API}
+#' @seealso \code{\link{steam_api_service_definitions}} for service definition
+#'    constants.
+NULL
+
+#' @export
+#' @name steam_api_args
+ARG_APP_ID <- "?appid="
+
+#' @export
+#' @name steam_api_args
+ARG_GAME_ID <- "?gameid="
+
+#' @export
+#' @name steam_api_args
+ARG_MAX_LENGTH <- "&maxlength="
+
+#' @export
+#' @name steam_api_args
+ARG_COUNT <- "&count="
+
+#' @export
+#' @name steam_api_args
+ARG_FORMAT <- "&format=JSON"
