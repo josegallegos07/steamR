c58aef6be1c439dd1981900f70be5dee20aa2dc5
josegallegos07 <jose.gallegos@brainplasticity.com>
2014-12-07 17:23:57 -0800

    ?steamR

---
+++ R/steamR.R
@@ -0,0 +1,9 @@
+#' An interface to Steam's Web API
+#'
+#' This package provides a wrapper for Steam's Web API.
+#'
+#' @docType package
+#' @name steamR
+#' @title steamR
+#' @author Jose Gallegos, \email{josegallegos07@@gmail.com}
+NULL
