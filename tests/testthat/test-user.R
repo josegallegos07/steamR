30c87193977bddee147f1e266e0a7d40c8dd32b0
josegallegos07 <jose.gallegos@brainplasticity.com>
2014-12-22 22:41:47 -0800

    fix arguments

--- tests/testthat/test-user.R
+++ tests/testthat/test-user.R
@@ -1,6 +1,6 @@
 context("user info")
 
 test_that("can get steam64 id for user", {
-  id = get_steam_id_64("earthr0t69")
+  id = get_steam_id_64(STEAM_KEY, "earthr0t69")
   expect_equal(id, EARTHR0T69)
 })
