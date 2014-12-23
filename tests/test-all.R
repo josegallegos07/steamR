eca5b88955a6eb84fdc522ddb6b277fc8e6c125c
josegallegos07 <jose.gallegos@brainplasticity.com>
2014-12-22 20:51:38 -0800

    try this again

--- tests/test-all.R
+++ tests/test-all.R
@@ -3,11 +3,13 @@ library(steamR)
 
 context("credentials")
 
+key <- function () {
+  key = base64enc::base64decode(Sys.getenv("STEAM_KEY"))
+  return (as.character(key)) 
+}
+
 test_that("can access key from travis", {
-
-  key = Sys.getenv("STEAM_KEY")
-  expect_equal(nchar(key), 32)
-
+  expect_equal(nchar(key()), 32)
 })
 
 test_check("steamR")
