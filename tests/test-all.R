fd36512c8243b63d821a44e528afb6eb152e2646
josegallegos07 <jose.gallegos@brainplasticity.com>
2014-12-22 21:56:24 -0800

    try accessing key again...

--- tests/test-all.R
+++ tests/test-all.R
@@ -7,9 +7,9 @@ get_env <- function (var) {
   return (as.character(Sys.getenv(var)))
 }
 
-# key <- function () {
-#   return (get_env("STEAM_KEY")) 
-# }
+key <- function () {
+  return (get_env("STEAM_KEY")) 
+}
 
 test <- function() {
   return (get_env("TEST")) 
@@ -19,8 +19,8 @@ test_that("can access env variable from travis", {
   expect_equal(nchar(test()), 4)
 })
 
-# test_that("can access key from travis", {
-#   expect_equal(nchar(key()), 32)
-# })
+test_that("can access key from travis", {
+  expect_equal(nchar(key()), 32)
+})
 
 test_check("steamR")
