9943cbf4d11d7931aa678555df68a2c4564968f5
josegallegos07 <jose.gallegos@brainplasticity.com>
2014-12-22 22:49:24 -0800

    added test for get_friend_list

--- tests/testthat/test-user.R
+++ tests/testthat/test-user.R
@@ -4,3 +4,8 @@ test_that("can get steam64 id for user", {
   id = get_steam_id_64(STEAM_KEY, "earthr0t69")
   expect_equal(id, EARTHR0T69)
 })
+
+test_that("can get friend list for user", {
+  list = get_friend_list(STEAM_KEY, EARTHR0T69, "all")
+  expect_more_than(length(list), 0)
+})
