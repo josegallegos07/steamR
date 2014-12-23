9b5dc04c176a9a14b0e6736a28800098134986e5
josegallegos07 <jose.gallegos@brainplasticity.com>
2014-12-22 22:52:38 -0800

    added tests for get_player_achievements & get_user_stats_for_game

--- tests/testthat/test-user.R
+++ tests/testthat/test-user.R
@@ -9,3 +9,13 @@ test_that("can get friend list for user", {
   list = get_friend_list(STEAM_KEY, EARTHR0T69, "all")
   expect_more_than(length(list), 0)
 })
+
+test_that("can get player achievements", {
+  achievements = get_player_achievements(STEAM_KEY, EARTHR0T69, REBIRTH)
+  expect_more_than(length(achievements), 0)
+})
+
+test_that("can get user stats for game", {
+  stats = get_user_stats_for_game(STEAM_KEY, EARTHR0T69, REBIRTH)
+  expect_more_than(length(stats), 0)
+})
