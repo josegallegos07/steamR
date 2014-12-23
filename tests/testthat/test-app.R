19d6115609ee83513d5529c7b1de38b9c8fc870d
josegallegos07 <jose.gallegos@brainplasticity.com>
2014-12-22 22:37:39 -0800

    added test for get_global_achievement_percentages_for_app

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -4,3 +4,8 @@ test_that("can get news for app", {
   news = get_news_for_app(REBIRTH)
   expect_more_than(length(news), 0)
 })
+
+test_that("can get global achievement percentages for app", {
+  percentages = get_global_achievement_percentages_for_app(REBIRTH)
+  expect_more_than(length(percentages), 0)
+})
