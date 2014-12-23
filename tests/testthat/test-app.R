a943b26ba025e521d213f76e2204e63920738e1d
josegallegos07 <jose.gallegos@brainplasticity.com>
2014-12-22 23:03:06 -0800

    added tests for get_schema_for_game, get_schema_name, get_schema_version, get_schema_achievements, get_schema_stats

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -9,3 +9,28 @@ test_that("can get global achievement percentages for app", {
   percentages = get_global_achievement_percentages_for_app(REBIRTH)
   expect_more_than(length(percentages), 0)
 })
+
+test_that("can get schema for app", {
+  schema = get_schema_for_game(STEAM_KEY, REBIRTH)
+  expect_more_than(length(schema), 0)
+})
+
+test_that("can get schema name for app", {
+  schema_name = get_schema_name(STEAM_KEY, REBIRTH)
+  expect_more_than(length(schema_name), 0)
+})
+
+test_that("can get schema version for app", {
+  schema_version = get_schema_version(STEAM_KEY, REBIRTH)
+  expect_more_than(length(schema_version), 0)
+})
+
+test_that("can get schema achievements for app", {
+  schema_achievements = get_schema_achievements(STEAM_KEY, REBIRTH)
+  expect_more_than(length(schema_achievements), 0)
+})
+
+test_that("can get schema stats for app", {
+  schema_stats = get_schema_stats(STEAM_KEY, REBIRTH)
+  expect_more_than(length(schema_stats), 0)
+})
