3921f5c0a974449f586b88e6ef7a97f787a6434e
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-04-22 19:56:47 -0700

    cleanup example script

--- examples/binding_of_isaac_rebirth.R
+++ examples/binding_of_isaac_rebirth.R
@@ -1,16 +1,16 @@
-# sample script a user's Binding of Isaac: Rebirth achievements & missing achievements
+# get a user's Binding of Isaac: Rebirth achievements & missing achievements
 
 rm(list = ls())
 
 library(steamR)
 
-STEAM_KEY = "GET_ONE!"
-my_id = get_steam_id_64(STEAM_KEY, "earthr0t69")
-rebirth_id = 250900
+STEAM_KEY = "FAKEKEY1234"
+USER_ID = get_steam_id_64(STEAM_KEY, "earthr0t69")
+REBIRTH_ID = 250900
 
-rebirth_schema = get_schema_achievements(STEAM_KEY, rebirth_id)
-global_achv = get_global_achievement_percentages_for_app(rebirth_id)
-my_achv = get_player_achievements(STEAM_KEY, my_id, rebirth_id)
+rebirth_schema = get_schema_achievements(STEAM_KEY, REBIRTH_ID)
+global_achv = get_global_achievement_percentages_for_app(REBIRTH_ID)
+my_achv = get_player_achievements(STEAM_KEY, USER_ID, REBIRTH_ID)
 
 # achievements relative to global
 relative_achv = sapply(rebirth_schema, function(x) {
